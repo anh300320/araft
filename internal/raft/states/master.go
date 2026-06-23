3c7b5dca33b3ed5fd2a157b17371a13974c8ec7d
Andrew Ngo <ngohoanganh300320@gmail.com>
2026-06-23 14:32:23 +0700

    feat: prevote handling for master

--- internal/raft/states/master.go
+++ internal/raft/states/master.go
@@ -4,44 +4,50 @@ import (
 	"github.com/anh300320/araft/internal/raft"
 	"github.com/anh300320/araft/internal/raft/common"
 	"github.com/anh300320/araft/internal/raft/protocol"
-	"github.com/anh300320/araft/internal/raft/transport"
 )
 
 type Master struct {
 	// Volatile states for leaders
+	raft *raft.Raft
+
 	nextIndex  []common.LogIndex
 	matchIndex []common.LogIndex
-
-	transport transport.Transport
-	followers []Follower
 }
 
-func (m Master) Run() {
+func (m *Master) Run() {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m Master) GetTransition() chan raft.State {
+func (m *Master) GetTransition() chan raft.State {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m Master) HandleHeartBeat(request protocol.AppendEntriesRequest) (protocol.AppendEntriesResponse, error) {
+func (m *Master) HandleHeartBeat(request protocol.AppendEntriesRequest) (protocol.AppendEntriesResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m Master) HandleAppendEntries(request protocol.AppendEntriesRequest) (protocol.AppendEntriesResponse, error) {
+func (m *Master) HandleAppendEntries(request protocol.AppendEntriesRequest) (protocol.AppendEntriesResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m Master) HandleVote(request protocol.VoteRequest) (protocol.VoteResponse, error) {
+func (m *Master) HandleVote(request protocol.VoteRequest) (protocol.VoteResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m Master) HandlePreVote(request protocol.PreVoteRequest) (protocol.PreVoteResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (m *Master) HandlePreVote(request protocol.PreVoteRequest) (protocol.PreVoteResponse, error) {
+	isGreaterTerm := request.HypotheticalTerm > m.raft.GetCurrentTerm()
+
+	latestLogEntry := m.raft.GetLatestLogEntry()
+	isLogUpToDate := latestLogEntry.Term < request.LastLogTerm ||
+		(latestLogEntry.Term == request.LastLogTerm && latestLogEntry.Id <= request.LastLogIndex)
+
+	return protocol.PreVoteResponse{
+		Term:    m.raft.GetCurrentTerm(),
+		Granted: isGreaterTerm && isLogUpToDate,
+	}, nil
 }
